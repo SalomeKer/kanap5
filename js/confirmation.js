acb15d7427a639f942bc7df3513302a848d7627b
SalomeKer <101712592+SalomeKer@users.noreply.github.com>
2022-06-14 12:34:42 +0200

    modification string page confirmation$

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,6 +1,6 @@
 
-let str = window.location.href;
-let url = new URL(str);
+let string = window.location.href;
+let url = new URL(string);
 let orderId = url.searchParams.get("id");
 document.getElementById("orderId").innerHTML = `${orderId}`;
 
